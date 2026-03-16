82294f0fb2824d7f858f8a9a51285b17fd3b8e9d
Rutvi Parakhiya <rparakhiya2007@gmail.com>
2026-03-16 14:07:57 +0530

    Delete server directory

--- server/routes/goals.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getGoal, setGoal } = require('../controllers/goalController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/').get(protect, getGoal).post(protect, setGoal);
-
-module.exports = router;
